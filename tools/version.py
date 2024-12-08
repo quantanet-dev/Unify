ed3adacb258ff0c395c43ce51596f70035ca4123
Quantanet.Dev <quantanet.dev@gmail.com>
2024-12-08 18:56:26 -0500

    fixed git submodule auto-fetch feature using Unify Build Tools on Windows;

--- tools/version.py
+++ tools/version.py
@@ -1,6 +1,6 @@
 import globals, sys, os
 
-print("Unify2 Build Tools v{}.{}".format(globals.TOOLSET_VERSION_MAJOR, globals.TOOLSET_VERSION_MINOR))
+print("Unify Build Tools v{}.{}".format(globals.TOOLSET_VERSION_MAJOR, globals.TOOLSET_VERSION_MINOR))
 print("Platform: {}-{}".format(globals.PLATFORM, globals.ARCHITECTURE))
 
 sys.exit(0)
