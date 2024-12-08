8eac1977f8839eafa26b4b1ac4f185977d02ceef
Quantanet.Dev <quantanet.dev@gmail.com>
2024-12-07 23:08:19 -0500

    cleaned up build tools to make them easier to use;

--- tools/gen.py
+++ tools/gen.py
@@ -3,18 +3,24 @@ import globals, subprocess, sys
 ret = 0
 
 if globals.IsWindows():
-    ret = subprocess.call(["cmd.exe", "/c", "premake\\premake5", "gmake2"])
+    ret = subprocess.call(["cmd.exe", "/c", "git submodule init || git submodule update"])
     if ret == 0:
-        ret = subprocess.call(["cmd.exe", "/c", "premake\\premake5", "vs2022"])
+        ret = subprocess.call(["cmd.exe", "/c", "premake\\premake5", "gmake2"])
+        if ret == 0:
+            ret = subprocess.call(["cmd.exe", "/c", "premake\\premake5", "vs2022"])
 
 if globals.IsLinux():
-    ret = subprocess.call(["chmod", "+x", "premake/premake5.linux"])
+    ret = subprocess.call(["git submodule init && git submodule update"])
     if ret == 0:
-        ret = subprocess.call(["premake/premake5.linux", "gmake2"])
+        ret = subprocess.call(["chmod", "+x", "premake/premake5.linux"])
+        if ret == 0:
+            ret = subprocess.call(["premake/premake5.linux", "gmake2"])
 
 if globals.IsMacos():
-    ret = subprocess.call(["chmod", "+x", "premake/premake5"])
+    ret = subprocess.call(["git submodule init && git submodule update"])
     if ret == 0:
-        ret = subprocess.call(["premake/premake5", "gmake2"])
+        ret = subprocess.call(["chmod", "+x", "premake/premake5"])
+        if ret == 0:
+            ret = subprocess.call(["premake/premake5", "gmake2"])
 
 sys.exit(ret)
